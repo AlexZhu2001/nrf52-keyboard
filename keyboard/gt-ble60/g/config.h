51691c352b314dd3133a2ee9c97e0e262dd9b8c8
Alex <AlexZhu2001@stu.xupt.edu.cn>
2022-07-26 17:07:32 +0800

    Modify Name

--- keyboard/gt-ble60/g/config.h
+++ keyboard/gt-ble60/g/config.h
@@ -26,8 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CONF_VENDOR_ID 0x4366 /* 配置项目内显示的VendorID */
 #define CONF_PRODUCT_ID 0x031B /* 配置项目内显示的ProductID */
 #define DEVICE_VER 0x0001 /* 硬件版本 */
-#define MANUFACTURER "Glab" /* 硬件制造商，用于蓝牙显示 */
-#define PRODUCT "GT BLE60G" /* 硬件名词，用于蓝牙显示 */
+#define MANUFACTURER "Alex" /* 硬件制造商，用于蓝牙显示 */
+#define PRODUCT "Alex's Keyboard" /* 硬件名词，用于蓝牙显示 */
 #define MACADDR_SEPRATOR ' ' /* 蓝牙名称后地址的分隔符。若不设置则不显示蓝牙名称后面的地址 */
 
 /* USB HID report parameter */
